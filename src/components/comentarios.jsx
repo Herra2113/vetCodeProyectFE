0a72b09ae559e379d5a3ea6889f0dfea7d4ba3e1
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-01 00:18:32 -0300

    feature: se agrego comentario

---
+++ src/components/comentarios.jsx
@@ -0,0 +1,11 @@
+const Comentario = () => {
+    return (
+      <div>
+        <h3 className="text-center">Diego J. Olmi</h3>
+        <h4>@DiegoOlmi912</h4>
+        <p>Muy buena la atencion, excelente trato de los medicos profesionales y muy linda pagina!</p>
+      </div>
+    )
+  }
+  
+  export default Comentario;

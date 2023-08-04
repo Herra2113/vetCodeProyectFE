7849552fc9b53a1c0e63076da10ae951759d47bc
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-04 03:14:12 -0300

    Feature: publicidad completa y detalles realizado s

--- src/pages/index/IndexPages.jsx
+++ src/pages/index/IndexPages.jsx
@@ -1,11 +1,13 @@
 import CarouselIndex from "../../components/Carousel/CarouselIndex";
 import Comentarios from "../../components/Comentarios/Comentarios";
+import Publicidad from "../../components/Publicidad/Publicidad";
 
 const IndexPages = () => {
 	return(
     <>
       < CarouselIndex /> 
       <Comentarios />
+      <Publicidad />
     </>
 	);
 }

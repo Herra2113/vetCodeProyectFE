b9349c18b89998097196946b0a1cd26c5dc92db7
Franco Paz <franco.paz.712@gmail.com>
2023-08-04 17:23:26 -0300

    Feature: se agrega planes de cobertura para mascotas

--- src/pages/index/IndexPages.jsx
+++ src/pages/index/IndexPages.jsx
@@ -1,10 +1,12 @@
 import CarouselIndex from "../../components/Carousel/CarouselIndex";
 import Comentarios from "../../components/Comentarios/Comentarios";
+import CardPlanes from "../../components/Planes/CardPlanes";
 
 const IndexPages = () => {
 	return(
     <>
       < CarouselIndex /> 
+      <CardPlanes />
       <Comentarios />
     </>
 	);

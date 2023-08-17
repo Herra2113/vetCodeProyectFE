7033ab4e84f951a6ec3258454dc661784e3465d3
fran villafane <villafanef57@gmail.com>
2023-08-17 13:52:00 -0300

    Feature/enlace de cada plan

--- src/components/Detallesplanmadurando/DetallesplanMadurando.jsx
+++ src/components/Detallesplanmadurando/DetallesplanMadurando.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Formulario from "../Detallesplancachorros/Formulario";
 import CoberturasyServicios from "../Detallesplancachorros/CoberturasyServicios";
 
+
 const DetallesplanMadurando = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const DetallesplanMadurando = () => {
         </h1>
         <CoberturasyServicios />
         <Formulario />
+        
       </div>
     </>
   );

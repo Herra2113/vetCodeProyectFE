f93d1f61ba793fde84330c7384d5add2c1b84e7d
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-16 23:47:28 -0300

    Feature: realizacion del footer y detalles completados

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styleFooter.css";
-import logodepagina from '../../assets/Images/logodepagina.jpg'
+import logodepagina from "../../assets/Images/logoicono.png";
 
 const Footer = () => {
   return (
-    <div className="container-fluid row" id="footer">
-      <div className="col-sm-12 col-md-4 col-lg-3">
-        <h3>Nosotros</h3>
+    <div className="row container-fluid" id="footer">
+      <div className="col-sm-12 col-md-4 col-lg-3 text-center">
         <ul>
+          <h3 className="mt-5 text-dark">Nosotros</h3>
           <li>
             <a href="">Quienes somos</a>
           </li>
@@ -25,11 +25,11 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="col-sm-12 col-md-4 col-lg-3">
-        <h3>Ayuda</h3>
+      <div className="col-sm-12 col-md-4 col-lg-3 text-center">
         <ul>
+          <h3 className="mt-5">Ayuda</h3>
           <li>
-            <a href="">¿Como comprar?</a>
+            <a href="">¿Cómo comprar?</a>
           </li>
           <li>
             <a href="">Preguntas frecuentes</a>
@@ -45,8 +45,28 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="col-sm-12 col-md-4 col-lg-3">
-        <img src={logodepagina} alt="logo de la pagina" />
+      <div className="col-sm-12 col-md-4 col-lg-4 text-center">
+        <a href="" className="iconoA">
+          <img
+            src={logodepagina}
+            alt="logo de la pagina"
+            id="imagenlogodefooter"
+          />
+        </a>
+      </div>
+      <div className="copyrightFooter">
+        <h6 className="copyrightFooterText">©2023-VetCode todos los derechos reservados.</h6>
+        <div className="iconosFooter">
+          <a href="">
+            <i class="bi bi-instagram"></i>
+          </a>
+          <a href="">
+            <i class="bi bi-whatsapp"></i>
+          </a>
+          <a href="">
+            <i class="bi bi-facebook"></i>
+          </a>
+        </div>
       </div>
     </div>
   );

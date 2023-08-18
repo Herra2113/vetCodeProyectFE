ce84615a73b90ada8d5d0d137443462057118260
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-17 22:14:17 -0300

    Feature:Cambios en detalles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,19 +3,19 @@ import "./styleLogin.css";
 
 const Login = () => {
   return (
-    <div className="container-fluid row" id="login">
+    <div className="row" id="login">
       <div className="col-3 d-flex justify-content-center" id="inicioSesion">
         <form action="">
-          <h2 className="text-center mt-4">Inicie sesion</h2>
-          <div className="contenedorDeInput">
+          <h2 className="text-center">Inicie sesion</h2>
+          <div className="contenedorDeInput container">
             <label htmlFor="usuario">Usuario</label>
-            <input type="text" id="usuario" placeholder="UsuarioVet"/>
+            <input type="text" id="usuario" placeholder="UsuarioVet" className="form-control"/>
           </div>
-          <div className="contenedorDeInput">
+          <div className="contenedorDeInput container mt-2">
             <label htmlFor="contraseña">Contraseña</label>
-            <input type="password" id="contraseña" placeholder="ConstraseñaVet"/>
+            <input type="password" id="contraseña" placeholder="ConstraseñaVet" className="form-control"/>
           </div>
-          <button type="submit" className="mx-5 mt-3 btn btn-primary" id="botonInicioSesion">Acceder</button>
+          <button type="submit" className="mx-5 mt-3 btn btn-outline-primary" id="botonInicioSesion">Acceder</button>
         </form>
       </div>
     </div>

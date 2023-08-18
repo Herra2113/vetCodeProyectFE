a36d77c2c8fd1e7981e6ca9a26935bf645d49274
Carlos Javier <carlos21201068@gmail.com>
2023-08-17 21:50:51 -0300

    primer nabvar sin css y con incono

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navegacion from './components/common/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navegacion></Navegacion>
+    </>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap-icons/font/bootstrap-icons.css"
-import IndexPages from './pages/index/indexPages';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <IndexPages />,
-  },
-  
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    
+    <App />
   </React.StrictMode>,
-);
+)

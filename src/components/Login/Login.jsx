324b85818930404893c0e67af4fc6f2fb48f42d5
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-17 22:26:43 -0300

    Feature:Cambios en el login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,17 +5,17 @@ const Login = () => {
   return (
     <div className="row" id="login">
       <div className="col-3 d-flex justify-content-center" id="inicioSesion">
-        <form action="">
-          <h2 className="text-center">Inicie sesion</h2>
-          <div className="contenedorDeInput container">
+        <form action="" className="formcontrolinicio">
+          <h2 className="text-center textoiniciosesion">Inicio Sesion</h2>
+          <div className="contenedorDeInput container text-center">
             <label htmlFor="usuario">Usuario</label>
-            <input type="text" id="usuario" placeholder="UsuarioVet" className="form-control"/>
+            <input type="text" id="usuario" placeholder="UsuarioVet" className="form-control" maxLength={5} minLength={5}/>
           </div>
-          <div className="contenedorDeInput container mt-2">
+          <div className="contenedorDeInput container mt-2 text-center">
             <label htmlFor="contraseña">Contraseña</label>
-            <input type="password" id="contraseña" placeholder="ConstraseñaVet" className="form-control"/>
+            <input type="password" id="contraseña" placeholder="ConstraseñaVet" className="form-control" maxLength={5} minLength={5}/>
           </div>
-          <button type="submit" className="mx-5 mt-3 btn btn-outline-primary" id="botonInicioSesion">Acceder</button>
+          <button type="submit" className="mt-3 mb-3 btn btn-outline-primary botondelinicio" id="botonInicioSesion">Acceder</button>
         </form>
       </div>
     </div>

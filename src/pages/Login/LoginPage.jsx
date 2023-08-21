8212690e6f19ff0157902144951dcc7a6632b382
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-21 14:39:57 -0300

    Feature: realizacion de login

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "../../components/Login/Login";
 import Footer from "../../components/Footer/Footer";
+import Navegacion from "../../components/common/Navbar"
 
 const LoginPage = () => {
   return (
     <>
+      <Navegacion />
       <Login />
       <Footer />
     </>

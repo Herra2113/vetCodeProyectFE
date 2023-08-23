44fe7f1d45b81ed35e08826a662dec510e28c410
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-23 14:21:11 -0300

    Feature: Cambios en login y validacion

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,23 +1,38 @@
-import { Nav, Container, Navbar } from 'react-bootstrap'
-import './Navbar.css'
-import logo from '/logo.png'
-import { Link } from 'react-router-dom'
+import { Nav, Container, Navbar } from "react-bootstrap";
+import "./Navbar.css";
+import logo from "/logo.png";
+import { Link } from "react-router-dom";
 
 const Navegacion = () => {
-	return (
-		<Navbar expand='lg' id='navbar'>
-			<Container>
-				<Navbar.Brand href='/'><img src={logo} className='imagenLogo'></img></Navbar.Brand>
-				<Navbar.Toggle area-aria-controls='basic-navbar-var'></Navbar.Toggle>
-				<Navbar.Collapse id='basic-navbar-var'>
-					<Nav className='ms-auto'>
-						<Link className='mx-3' to="/Admin" >Administrador</Link>
-						<Link to='/Login'>Ingresar</Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	)
-}
+  return (
+    <Navbar expand="lg" id="navbar">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} className="imagenLogo"></img>
+        </Navbar.Brand>
+        <Navbar.Toggle area-aria-controls="basic-navbar-var"></Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-var">
+          <Nav className="ms-auto linksNavbar">
+            <Link className="mx-2" to="/Admin">
+              <i className="bi bi-shield-plus mx-1"></i>Administrador
+            </Link>
+            <Link className="mx-2" to="/">
+              <i className="bi bi-house mx-1 mx-1"></i>Inicio
+            </Link>
+            <Link className="mx-2" to="/Nosotros">
+              <i className="bi bi-info-circle mx-1"></i>Sobre Nosotros
+            </Link>
+            <Link className="mx-2" to="/Contacto">
+              <i className="bi bi-envelope-plus mx-1"></i>Contacto
+            </Link>
+            <Link className="mx-2" to="/Login">
+              <i className="bi bi-person-circle mx-1"></i>Ingresar
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Navegacion
+export default Navegacion;
